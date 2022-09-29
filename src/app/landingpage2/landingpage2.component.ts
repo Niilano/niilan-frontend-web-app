b9561a48b0594984952ea0a8e7d61ce0813fbcf6
latif83 <latifm8360@gmail.com>
2022-09-29 03:40:43 +0000

    Changing home page

--- src/app/landingpage2/landingpage2.component.ts
+++ src/app/landingpage2/landingpage2.component.ts
@@ -10,6 +10,36 @@ export class Landingpage2Component implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const menu = document.querySelector('.menu-bar p') as HTMLElement
+    const logo = document.querySelector('.logo') as HTMLElement
+    const header = document.querySelector('header') as HTMLElement
+    const mnav = document.querySelectorAll('#m-nav a')
+    const nav = document.querySelector('.desktop') as HTMLElement
+window.addEventListener('scroll', fixNav)
+
+function fixNav() {
+    if(window.scrollY > header.offsetHeight + 150) {
+        header.style.backgroundColor = 'white';
+        header.classList.add('shadow')
+        nav.style.color = 'black';
+        logo.style.color = 'black';
+        menu.style.color = 'black';
+        mnav.forEach((a:any)=>{
+          a.style.color = 'black';
+          a.style.borderColor = 'black';
+        })
+    } else {
+       header.style.backgroundColor = '';
+       header.classList.remove('shadow')
+        nav.style.color = '';
+        logo.style.color = '';
+        menu.style.color = '';
+        mnav.forEach((a:any)=>{
+          a.style.color = '';
+          a.style.borderColor = '';
+        })
+    }
+}
   }
 
 }
