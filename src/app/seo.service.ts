4904d257260c541a06d1642026c72b5f438575e6
latif83 <latifm8360@gmail.com>
2022-10-23 19:40:51 +0100

    seo - twitter

--- src/app/seo.service.ts
+++ src/app/seo.service.ts
@@ -13,7 +13,11 @@ export class SeoService {
       {name: 'Keywords', content : 'Niilano car rentals, Car rentals based in Ghana, Car rentals based in Gh, Ghana car rentals, Rent a car in Ghana, Rent a car, Rent a vehicle, book cars in Ghana, Book a car in Ghana'},
       { name: 'description', content: 'A car rental website based in Ghana. Niilano helps you get access to different varieties of cars to book for renting.'},
       { name: 'robots', content: 'index,follow'},
-      {name:'author', content: 'Abdul-Latif Mohammed'}
+      {name:'author', content: 'Abdul-Latif Mohammed'},
+      {name:'twitter:card', content: 'summary'},
+      {name:'twitter:image', content: 'http'},
+      {name:'twitter:title', content: 'title'},
+      {name:'twitter:description', content: 'description'}
     ])
   }
 
