5a50b62b4e2e196be607a784cee48833161d0457
latif83 <latifm8360@gmail.com>
2022-12-24 18:45:21 +0000

    changed api key url from railway to render

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   // apiKey: 'https://niilan.herokuapp.com/',
-  apiKey: 'https://niilano.up.railway.app/',
+  // apiKey: 'https://niilano.up.railway.app/',
+  apiKey: 'https://niilano-backend.onrender.com/',
   token : ''
 };
